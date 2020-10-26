e5efeff80c7e6b47136649338d938f6ea2bd2431
princesshally <58790833+princesshally@users.noreply.github.com>
2020-10-26 19:01:33 -0400

    Update vars.tf

--- modules/ec2/vars.tf
+++ modules/ec2/vars.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 variable "ec2_count" {
   default = 1
 }
